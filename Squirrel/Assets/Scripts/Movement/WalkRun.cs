a6ef3e03132123ce625dd6e620d36d5075f640e7
xcyl0 <jacobloat@gmail.com>
2025-12-01 17:35:06 -0500

    Basic Movement
    Needs better sense of momentum and a climbing mechanic

--- Squirrel/Assets/Scripts/Movement/WalkRun.cs
+++ Squirrel/Assets/Scripts/Movement/WalkRun.cs
@@ -3,8 +3,12 @@ using UnityEngine.InputSystem;
 public class WalkRun : MonoBehaviour
 {
     public InputAction move;
+    public InputAction jump;
+    public InputAction sprint;
     public Vector2 moveDirection;
     public float acceleration;
+    public float walkSpeed;
+    public float runSpeed;
     public float maxSpeed;
     public float jumpForce;
     public Rigidbody2D rb;
@@ -12,7 +16,35 @@ public class WalkRun : MonoBehaviour
     public void Awake()
     {
         move = InputSystem.actions.FindAction("Move");
+        jump = InputSystem.actions.FindAction("Jump");
+        sprint = InputSystem.actions.FindAction("Sprint");
+
+        
+
         rb = GetComponent<Rigidbody2D>();
+        maxSpeed = walkSpeed;
+    }
+
+    private void OnEnable()
+    {
+        move.Enable();
+        jump.Enable();
+        sprint.Enable();
+
+        jump.performed += Jump;
+        sprint.started += Sprint;
+        sprint.canceled += Walk;
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        jump.Disable();
+        sprint.Disable();
+
+        jump.performed -= Jump;
+        sprint.started -= Sprint;
+        sprint.canceled -= Walk;
     }
 
     private void Update()
@@ -20,11 +52,38 @@ public class WalkRun : MonoBehaviour
         moveDirection = move.ReadValue<Vector2>();
         if (move.ReadValue<Vector2>().x == 0)
         {
-            rb.linearVelocityX = Mathf.MoveTowards(rb.linearVelocity.x, 0, acceleration * Time.deltaTime);
+            Stop();
             return;
+        } else
+        {
+            Move();
         }
+    }
+
+    public void Move()
+    {
         rb.AddForce(new Vector2(moveDirection.x * acceleration * Time.deltaTime, 0));
         rb.linearVelocityX = Mathf.Clamp(rb.linearVelocity.x, -maxSpeed, maxSpeed);
         Debug.Log(rb.linearVelocity);
+    }
+
+    public void Stop()
+    {
+        rb.linearVelocityX = Mathf.MoveTowards(rb.linearVelocity.x, 0, 4 * maxSpeed * Time.deltaTime);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+
+    public void Sprint(InputAction.CallbackContext context)
+    {
+        maxSpeed = runSpeed;
+    }
+
+    public void Walk(InputAction.CallbackContext context)
+    {
+        maxSpeed = walkSpeed;
     }
 }

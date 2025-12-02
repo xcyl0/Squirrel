14e6177786751ceae825f7369c28100db0394b4e
xcyl0 <jacobloat@gmail.com>
2025-12-01 20:35:49 -0500

    Still floaty but climbing works to a degree

--- Squirrel/Assets/Scripts/Movement/WalkRun.cs
+++ Squirrel/Assets/Scripts/Movement/WalkRun.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Timeline;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class WalkRun : MonoBehaviour
@@ -10,8 +11,15 @@ public class WalkRun : MonoBehaviour
     public float walkSpeed;
     public float runSpeed;
     public float maxSpeed;
+    public float maxClimbSpeed;
     public float jumpForce;
+    public float stoppingPower;
+
+    public Collider2D clb;
     public Rigidbody2D rb;
+
+    public bool climbing;
+    public bool onsurface;
 
     public void Awake()
     {
@@ -23,6 +31,37 @@ public class WalkRun : MonoBehaviour
 
         rb = GetComponent<Rigidbody2D>();
         maxSpeed = walkSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Climbable")
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+            rb.gravityScale = 0.5f;
+            climbing = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Climbable")
+        {
+            rb.gravityScale = 1.5f;
+            climbing = false;
+        }     
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided with " + collision.gameObject.name + " on layer " + collision.gameObject.layer);
+        if (collision.gameObject.layer == 3)
+            onsurface = true;
+    }
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 3)
+            onsurface = false;
     }
 
     private void OnEnable()
@@ -50,31 +89,51 @@ public class WalkRun : MonoBehaviour
     private void Update()
     {
         moveDirection = move.ReadValue<Vector2>();
-        if (move.ReadValue<Vector2>().x == 0)
+        if (move.ReadValue<Vector2>() == new Vector2(0,0))
         {
             Stop();
             return;
         } else
         {
+            Flip();
             Move();
         }
     }
 
+    public void Flip()
+    {
+        if (moveDirection.x > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else if (moveDirection.x < 0)
+            transform.localScale = new Vector3(1, 1, 1);
+    }
     public void Move()
     {
-        rb.AddForce(new Vector2(moveDirection.x * acceleration * Time.deltaTime, 0));
-        rb.linearVelocityX = Mathf.Clamp(rb.linearVelocity.x, -maxSpeed, maxSpeed);
-        Debug.Log(rb.linearVelocity);
+        if (climbing)
+        {
+            rb.AddForce(new Vector2(moveDirection.x * acceleration * Time.deltaTime, moveDirection.y * acceleration * Time.deltaTime + Mathf.Abs(moveDirection.x) * acceleration * Time.deltaTime / 3));
+            rb.linearVelocityY = Mathf.Clamp(rb.linearVelocity.y, -maxClimbSpeed, maxClimbSpeed);
+            rb.linearVelocityX = Mathf.Clamp(rb.linearVelocity.x, -maxSpeed, maxSpeed);
+        } else
+        {
+            rb.AddForce(new Vector2(moveDirection.x * acceleration * Time.deltaTime, 0));
+            rb.linearVelocityX = Mathf.Clamp(rb.linearVelocity.x, -maxSpeed, maxSpeed);
+        }
     }
 
     public void Stop()
     {
-        rb.linearVelocityX = Mathf.MoveTowards(rb.linearVelocity.x, 0, 4 * maxSpeed * Time.deltaTime);
+        rb.linearVelocityX = Mathf.MoveTowards(rb.linearVelocity.x, 0, stoppingPower* Time.deltaTime);
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
+        if (!onsurface) return;
+        if (!climbing)  
         rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        else
+            rb.AddForce(new Vector2(jumpForce / 2 * transform.localScale.x, jumpForce * 1.5f), ForceMode2D.Impulse);
+
     }
 
     public void Sprint(InputAction.CallbackContext context)
